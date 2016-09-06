6c2a015d52ad73b9b7221e6b32cbf74b54d0716f
guohongxin24 <love_tomatoo@163.com>
2016-09-06 20:32:02 +0800

    将相机相册的代码放进 ExtendMediaPicker 类

--- HandlePermission_6.0/app/src/main/java/com/ghx/handlepermission_60/activity/MainActivity.java
+++ HandlePermission_6.0/app/src/main/java/com/ghx/handlepermission_60/activity/MainActivity.java
@@ -1,18 +1,7 @@
 package com.ghx.handlepermission_60.activity;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,24 +9,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.ghx.handlepermission_60.utils.BitmapUtils;
-import com.ghx.handlepermission_60.utils.MediaUtil;
+import com.ghx.handlepermission_60.utils.ExtendMediaPicker;
 import com.ghx.handlepermission_60.weiget.ActionSheetDialog;
 import com.ghx.handlepermission_60.R;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    //    来新公司马上三个月了，2.3的版本终于要上线了，真不容易啊，加班加班加班。我只想
     private static final int MYPERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE_ALBUM = 1;
     private static final int MYPERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE_PHOTOGRAPH = 2;
-    private static final int REQUEST_CODE_PICK_IMAGE = 100;
-    private static final int REQUEST_CODE_TAKE_PHOTO = 200;
-    private static final int REQUEST_CODE_CROP_PHOTO = 300;
     private ImageView mIvShow;
     private Button mBtnSelect;
-    private Uri imageUri;
-    private File tempFile;
+    private ExtendMediaPicker mMediaPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +31,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
         initData();
         initListener();
+
     }
 
     private void initView() {
         mIvShow = (ImageView) findViewById(R.id.iv_show);
         mBtnSelect = (Button) findViewById(R.id.btn_select);
 
-        tempFile = createTempFile();
     }
 
     private void initData() {
+
+        mMediaPicker = new ExtendMediaPicker(this);
+
         mIvShow.setImageResource(R.mipmap.default_null);
     }
 
     private void initListener() {
 
         mBtnSelect.setOnClickListener(this);
+
+        mMediaPicker.setOnPicBackListener(new ExtendMediaPicker.PicBackListener() {
+            @Override
+            public void setPic(String imagePath) {
+                //把图片的path回调回来————处理，将图片拿到__展示即可
+                //TODO...
+            }
+        });
     }
 
     @Override
@@ -85,23 +80,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             @Override
                             public void onClick(int which) {
 
-                                //判断权限
-                                if (isPermissionRequired(MainActivity.this,
-                                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                                    //如果需要权限，则去申请
-                                    ActivityCompat.requestPermissions(MainActivity.this,
-                                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                            MYPERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE_ALBUM);
-                                } else {
-                                    //做该做的事
-                                    Toast.makeText(getApplicationContext(),
-                                            "访问相册",
-                                            Toast.LENGTH_SHORT).show();
+//                                //判断权限
+//                                if (isPermissionRequired(MainActivity.this,
+//                                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//                                    //如果需要权限，则去申请
+//                                    ActivityCompat.requestPermissions(MainActivity.this,
+//                                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+//                                            MYPERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE_ALBUM);
+//                                } else {
+//                                    //做该做的事
+//                                    Toast.makeText(getApplicationContext(),
+//                                            "访问相册",
+//                                            Toast.LENGTH_SHORT).show();
 
-                                    openSystemPickImage();
-                                }
-
+                                mMediaPicker.showPickerView(false);
                             }
+
+//                            }
                         })
                 .addSheetItem("拍照", ActionSheetDialog.SheetItemColor.Blue,
                         new ActionSheetDialog.OnSheetItemClickListener() {
@@ -121,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                         "访问相机",
                                         Toast.LENGTH_SHORT).show();
 
-                                openSystemCamera();
+                                mMediaPicker.showPickerView(true);
 //                                }
                             }
                         })
@@ -142,7 +137,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (requestCode) {
             case MYPERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE_ALBUM:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openSystemPickImage();
+                    mMediaPicker.showPickerView(false);
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "需要权限，请在设置－>全部应用中打开",
@@ -151,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case MYPERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE_PHOTOGRAPH:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openSystemCamera();
+                    mMediaPicker.showPickerView(true);
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "需要权限，请在设置－>全部应用中打开",
@@ -167,136 +162,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private boolean isPermissionRequired(Context context, String permisssion) {
-
-        boolean isPermisssionReq = ContextCompat.checkSelfPermission(context,
-                permisssion) != PackageManager.PERMISSION_GRANTED;
-        return isPermisssionReq;
-
-    }
-
-    private void openSystemPickImage() {
-        Intent photoPickerIntent = null;
-
-        if (Build.VERSION.SDK_INT < 19) {
-            photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        } else {
-            photoPickerIntent = new Intent(
-                    Intent.ACTION_PICK,
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        }
-
-        photoPickerIntent.setDataAndType(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-        try {
-            startActivityForResult(photoPickerIntent,
-                    REQUEST_CODE_PICK_IMAGE);
-
-        } catch (Exception e) {
-
-            Toast.makeText(getApplicationContext(), "请检查你手机的系统相册", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    private void openSystemCamera() {
-        if (Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED)) {
-            File dirPath = new File(Environment.getExternalStorageDirectory(),
-                    System.currentTimeMillis() + ".jpg");
-            imageUri = Uri.fromFile(dirPath);
-
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(intent,
-                        REQUEST_CODE_TAKE_PHOTO);
-            }
-
-
-        } else {
-            Toast.makeText(getApplicationContext(),
-                    "Before you take photos please insert SD card",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (resultCode != Activity.RESULT_OK) {
-
-            return;
-        }
-
-        switch (requestCode) {
-            case REQUEST_CODE_PICK_IMAGE:
-                String path = MediaUtil.getPath(this, data.getData());
-
-                cropImageUri(Uri.fromFile(new File(path)), 300, 300,
-                        REQUEST_CODE_CROP_PHOTO);
-
-                break;
-            case REQUEST_CODE_TAKE_PHOTO:
-
-                cropImageUri(imageUri, 300, 300, REQUEST_CODE_CROP_PHOTO);
-
-                break;
-            case REQUEST_CODE_CROP_PHOTO:
-
-                String imagePath = tempFile.getAbsolutePath();
-
-                handImage(imagePath);
-
-                break;
-        }
-
-    }
-
-    private void handImage(String imagePath) {
-        Bitmap photo = BitmapUtils.decodeBitmap(imagePath);
-        mIvShow.setImageBitmap(photo);
-    }
-
-
-    private void cropImageUri(Uri uri, int outputX, int outputY, int requestCode) {
-        /*if (isCamera) {
-
-            sendMessage(uri.getPath());
-
-            return;
-        }*/
-
-        try {
-            // android1.6以后只能传图库中图片
-            Intent intent = new Intent("com.android.camera.action.CROP");
-            intent.setDataAndType(uri, "image/*");
-            intent.putExtra("crop", "true");
-            intent.putExtra("aspectX", 1);
-            intent.putExtra("aspectY", 1);
-            intent.putExtra("outputX", outputX);
-            intent.putExtra("outputY", outputY);
-            intent.putExtra("scale", true);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
-            intent.putExtra("return-data", false);
-            intent.putExtra("outputFormat",
-                    Bitmap.CompressFormat.JPEG.toString());
-            intent.putExtra("noFaceDetection", true);
-                startActivityForResult(intent, requestCode);
-
-        } catch (ActivityNotFoundException ex) {
-            Toast.makeText(getApplicationContext(), "Can not find image crop app", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    private File createTempFile() {
-        if (Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED)) {
-            return new File(Environment.getExternalStorageDirectory(),
-                    "image.jpg");
-        } else {
-            return new File(getFilesDir(), "image.jpg");
-        }
+        mMediaPicker.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ HandlePermission_6.0/app/src/main/java/com/ghx/handlepermission_60/utils/ExtendMediaPicker.java
@@ -0,0 +1,189 @@
+package com.ghx.handlepermission_60.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * Created by guo_hx on 2016/9/6.20:08
+ *
+ * 调用系统相机，系统相册类
+ *
+ */
+public class ExtendMediaPicker {
+
+    private static final int REQUEST_CODE_CROP_PHOTO = 2;
+    private static final int REQUEST_CODE_PICK_IMAGE = 3;
+    private static final int REQUEST_CODE_TAKE_PHOTO = 4;
+
+    private Uri imageUri;
+    private File tempFile;
+
+    private Activity mActivity;
+
+    PicBackListener mListener;
+
+    public ExtendMediaPicker(Activity activity) {
+
+        this.mActivity = activity;
+
+        this.tempFile = createTempFile();
+    }
+
+    public void showPickerView(boolean isCamera) {
+
+        if (isCamera) {
+
+            openSystemCamera();
+
+        } else {
+
+            openSystemPickImage();
+        }
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+
+            return;
+        }
+
+        switch (requestCode) {
+            case REQUEST_CODE_PICK_IMAGE:
+                String path = MediaUtil.getPath(mActivity, data.getData());
+
+                cropImageUri(Uri.fromFile(new File(path)), 300, 300,
+                        REQUEST_CODE_CROP_PHOTO);
+
+                break;
+            case REQUEST_CODE_TAKE_PHOTO:
+
+                cropImageUri(imageUri, 300, 300, REQUEST_CODE_CROP_PHOTO);
+
+                break;
+            case REQUEST_CODE_CROP_PHOTO:
+
+                String imagePath = tempFile.getAbsolutePath();
+
+                mListener.setPic(imagePath);
+
+                break;
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void openSystemPickImage() {
+        Intent photoPickerIntent = null;
+
+        if (Build.VERSION.SDK_INT < 19) {
+            photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        } else {
+            photoPickerIntent = new Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        }
+
+        photoPickerIntent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        try {
+            mActivity.startActivityForResult(photoPickerIntent,
+                    REQUEST_CODE_PICK_IMAGE);
+
+        } catch (Exception e) {
+
+            Toast.makeText(mActivity, "请检查你手机的系统相册", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void openSystemCamera() {
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            File dirPath = new File(Environment.getExternalStorageDirectory(),
+                    System.currentTimeMillis() + ".jpg");
+            imageUri = Uri.fromFile(dirPath);
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            if (intent.resolveActivity(mActivity.getPackageManager()) != null) {
+                mActivity.startActivityForResult(intent,
+                        REQUEST_CODE_TAKE_PHOTO);
+            }
+
+        } else {
+
+            Toast.makeText(mActivity, "Before you take photos please insert SD card", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File createTempFile() {
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            return new File(Environment.getExternalStorageDirectory(),
+                    "image.jpg");
+        } else {
+            return new File(mActivity.getFilesDir(), "image.jpg");
+        }
+    }
+
+    /**
+     * 截取图像部分区域
+     *
+     * @param uri
+     * @param outputX
+     * @param outputY
+     * @return
+     */
+    private void cropImageUri(Uri uri, int outputX, int outputY, int requestCode) {
+
+        try {
+            // android1.6以后只能传图库中图片
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            intent.setDataAndType(uri, "image/*");
+            intent.putExtra("crop", "true");
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("outputX", outputX);
+            intent.putExtra("outputY", outputY);
+            intent.putExtra("scale", true);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+            intent.putExtra("return-data", false);
+            intent.putExtra("outputFormat",
+                    Bitmap.CompressFormat.JPEG.toString());
+            intent.putExtra("noFaceDetection", true);
+            if (mActivity == null) {
+                mActivity.startActivityForResult(intent, requestCode);
+            } else {
+                mActivity.startActivityForResult(intent, requestCode);
+            }
+
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(mActivity, "Can not find image crop app", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**取到图片后的接口回调-----------------------------------------------------------------------------------*/
+
+    public void setOnPicBackListener(PicBackListener listener) {
+        this.mListener = listener;
+    }
+
+
+    public interface PicBackListener {
+        void setPic(String imagePath);
+
+    }
+
+
+}
